1095adad5c5d5767d453c796830c2f82b37f4734
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-20 17:32:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,13 @@
 def square_array(array)
   # your code here
-  new_array = []
-  array.each{ |e| new_array.push(e**2) }
-  new_array
+  #new_array = []
+  #array.each{ |e| new_array.push(e**2) }
+  #new_array
+
+  #array.inject([]) do |squares, e|
+  #  squares.push(e**2)
+  #  squares
+  #end
+
+  array.collect{ |e| e**2 }
 end
